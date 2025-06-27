6f48e48d5363edde0cba829c00eaae51e10b642d
aalejoz25 <alvaroalejo25@gmail.com>
2025-06-27 11:40:53 -0500

    feat: configuracion de conexion
    validacion de la conexion usando solamente el ORM

--- utils/db.go
+++ utils/db.go
@@ -1,14 +1,11 @@
 package utils
 
 import (
-	"database/sql"
 	"fmt"
 	"net/url"
 	"os"
 
 	"github.com/astaxie/beego"
-	"github.com/astaxie/beego/logs"
-	_ "github.com/lib/pq"
 	"github.com/udistrital/utils_oas/ssm"
 )
 
@@ -19,12 +16,6 @@ func InitDB() (connStr string, username string, password string, err error) {
 	}
 
 	connStr = buildConnStr(username, password)
-
-	err = validateDBConnection(connStr)
-	if err != nil {
-		return "", "", "", err
-	}
-	logs.Info("Database connection established successfully")
 	return connStr, username, password, nil
 }
 
@@ -40,19 +31,19 @@ func getDBCredentials() (string, string, error) {
 
 		username, err = ssm.GetParameterFromParameterStore(basePath + "username")
 		if err != nil {
-			return "", "", fmt.Errorf("error retrieving username: %v", err)
+			return "", "", fmt.Errorf("failed to retrieve username from SSM: %v", err)
 		}
 
 		password, err = ssm.GetParameterFromParameterStore(basePath + "password")
 		if err != nil {
-			return "", "", fmt.Errorf("error retrieving password: %v", err)
+			return "", "", fmt.Errorf("failed to retrieve password from SSM: %v", err)
 		}
 	} else {
 		username = os.Getenv("PGUSER")
 		password = os.Getenv("PGPASS")
 
 		if username == "" || password == "" {
-			return "", "", fmt.Errorf("error retrieving PGUSER and PGPASS from env variables")
+			return "", "", fmt.Errorf("missing PGUSER or PGPASS environment variables")
 		}
 	}
 	return username, password, nil
@@ -64,16 +55,4 @@ func buildConnStr(user, pass string) string {
 		beego.AppConfig.String("PGport") + "/" +
 		beego.AppConfig.String("PGdb") + "?sslmode=disable&search_path=" +
 		beego.AppConfig.String("PGschema")
-}
-
-func validateDBConnection(connStr string) error {
-	db, err := sql.Open("postgres", connStr)
-	if err != nil {
-		return fmt.Errorf("failed to open connection to the database: %v", err)
-	}
-	defer db.Close()
-	if err := db.Ping(); err != nil {
-		return fmt.Errorf("unable to establish connection to the database (ping failed): %v", err)
-	}
-	return nil
 }
